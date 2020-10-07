7aa5aad3bb05dc6daa910f441b24a303bbfb5946
Aaron Scofiled <Aaron_dhu@outlook.com>
2020-10-07 17:19:09 +0800

    revision

--- Palindrome.py
+++ Palindrome.py
@@ -9,10 +9,13 @@ if sLen%2==0:
 else:
     reverseStart=sLen//2
 
-for i,j in zip(s[fowardStart:fowardEnd],s[:reverseStart:-1]):
-    if flag==False: break
-    if i!=j:
-        flag=False
-        break
+#for i,j in zip(s[fowardStart:fowardEnd],s[:reverseStart:-1]):
+#    if flag==False: break
+#    if i!=j:
+#        flag=False
+#        break
+
+if s[fowardStart:fowardEnd]!=s[:reverseStart:-1]:
+    flag=False
 
 print(flag)
